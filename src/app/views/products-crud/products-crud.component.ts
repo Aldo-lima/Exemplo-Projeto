5eb7d250cd15b7fcb62fd899a6cc70380ba94887
Aldo Cesar <57098697+Aldo-lima@users.noreply.github.com>
2021-08-03 12:14:18 -0300

    iniciar projeto

---
+++ src/app/views/products-crud/products-crud.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
+
+@Component({
+  selector: 'app-products-crud',
+  templateUrl: './products-crud.component.html',
+  styleUrls: ['./products-crud.component.css']
+})
+export class ProductsCrudComponent implements OnInit {
+
+  constructor( private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  navigateToProductCreat(): void {
+    this.router.navigate(['products/create'])
+  }
+
+}
